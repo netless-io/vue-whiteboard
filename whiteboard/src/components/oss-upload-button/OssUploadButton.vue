c697265d01e62360043e402471db2705c20e1108
netless <meetcheerego@gmail.com>
2020-09-29 17:17:04 +0800

    uploadButton

--- whiteboard/src/components/oss-upload-button/OssUploadButton.vue
+++ whiteboard/src/components/oss-upload-button/OssUploadButton.vue
@@ -1,75 +1,28 @@
-<template>
-  <div class="oss-upload-box">
-    <el-upload>
+<template v-for="item of dataArr">
+  <div class="oss-upload-box" :key="item">
+    <el-upload
+      :key="index"
+      :accept="item.accept"
+      :show-file-list="false"
+      :http-request="[item.fuc]"
+    >
       <div class="oss-ipload-section">
         <div class="oss-upload-section-inner">
           <div class="oss-upload-title-section">
-            <div class="oss-upload-title">上传图片</div>
+            <div class="oss-upload-title">{{ item.title }}</div>
             <div class="oss-upload-icon">
-              <img :src="image" />
+              <img :src="item.img" />
             </div>
           </div>
           <div class="oss-upload-section-script">
             <div class="oss-upload-section-text">
-              支持常见格式，可以改变图片大小和位置
+              {{ text }}
             </div>
           </div>
         </div>
       </div>
     </el-upload>
     <div :style="{ width: 208, height: 0.5, backgroundColor: '#E7E7E7' }" />
-    <el-upload>
-      <div class="oss-upload-section">
-        <div class="oss-upload-section-inner">
-          <div class="oss-upload-title-section">
-            <div class="oss-upload-title">上传视频</div>
-            <div class="oss-upload-icon">
-              <img :src="Video" />
-            </div>
-          </div>
-          <div class="oss-upload-section-script">
-            <div class="oss-upload-section-text">
-              支持 MP4 格式
-            </div>
-          </div>
-        </div>
-      </div>
-    </el-upload>
-    <el-upload>
-      <div class="oss-upload-section">
-        <div class="oss-upload-section-inner">
-          <div class="oss-upload-title-section">
-            <div class="oss-upload-title">上传视频</div>
-            <div class="oss-upload-icon">
-              <img :src="Video" />
-            </div>
-          </div>
-          <div class="oss-upload-section-script">
-            <div class="oss-upload-section-text">
-              支持 MP4 格式
-            </div>
-          </div>
-        </div>
-      </div>
-    </el-upload>
-    <el-upload>
-      <div class="oss-upload-section">
-        <div class="oss-upload-section-inner">
-          <div class="oss-upload-title-section">
-            <div class="oss-upload-title">上传视频</div>
-            <div class="oss-upload-icon">
-              <img :src="Video" />
-            </div>
-          </div>
-          <div class="oss-upload-section-script">
-            <div class="oss-upload-section-text">
-              支持 MP4 格式
-            </div>
-          </div>
-        </div>
-      </div>
-    </el-upload>
-    <el-upload>
   </div>
 </template>
 
@@ -97,7 +50,55 @@ export default {
       uploadActive,
       fileTransWeb,
       Video,
-      Audio
+      Audio,
+      dataArr: [
+        [
+          { title: "上传图片" },
+          { title: "上传视频" },
+          { title: "上传音频" },
+          { title: "资料转网页" },
+          { title: "文档转图片" }
+        ],
+        [
+          nm,
+          { img: "image" },
+          { img: "Video" },
+          { img: "Audio" },
+          { img: "fileTransWeb" },
+          { img: "fileTransImg" }
+        ],
+        [
+          { text: "支持常见格式,可以改变图片大小和位置。" },
+          { text: "支持 MP4 格式" },
+          { text: "支持 MP3 格式" },
+          { text: "支持 pptx 格式，如果是 ppt 格式，请手动转换。" },
+          { text: "支持 ppt、pptx、word and pdf 格式" }
+        ],
+        [
+          { accept: "image/*" },
+          { accept: "video/mp4" },
+          { accept: "audio/mp3" },
+          {
+            accept:
+              "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+          },
+          {
+            accept:
+              "application/pdf, " +
+              "application/vnd.openxmlformats-officedocument.presentationml.presentation, " +
+              "application/vnd.ms-powerpoint, " +
+              "application/msword, " +
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+          },
+          [
+            { fun: "uploadImage" },
+            { fun: "uploadVideo" },
+            { fun: "uploadAudio" },
+            { fun: "uploadDynamic" },
+            { fun: "uploadStatic" }
+          ]
+        ]
+      ]
     };
   },
 
