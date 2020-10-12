6ea59dfe88f1396c7c998021cdb53b723eb75e6e
netless <meetcheerego@gmail.com>
2020-10-12 12:42:09 +0800

    upload

--- whiteboard/src/components/oss-upload-button/OssUploadButton.vue
+++ whiteboard/src/components/oss-upload-button/OssUploadButton.vue
@@ -28,7 +28,12 @@
         <div :style="{ width: 208, height: 0.5, backgroundColor: '#E7E7E7' }" />
       </div>
     </template>
-    <el-tooltip placement="right" title="upload">
+    <el-tooltip
+      placement="right"
+      title="upload"
+      slot="reference"
+      content="upload"
+    >
       <div class="oss-upload-cell-box-left">
         <div class="oss-upload-cell">
           <img :src="isActive ? uploadActive : upload" alt="upload" />
@@ -89,51 +94,41 @@ export default {
         bucket: this.oss.bucket
       }),
       dataArr: [
-        [
-          { title: "上传图片" },
-          { title: "上传视频" },
-          { title: "上传音频" },
-          { title: "资料转网页" },
-          { title: "文档转图片" }
-        ],
-        [
-          { img: "image" },
-          { img: "Video" },
-          { img: "Audio" },
-          { img: "fileTransWeb" },
-          { img: "fileTransImg" }
-        ],
-        [
-          { text: "支持常见格式,可以改变图片大小和位置。" },
-          { text: "支持 MP4 格式" },
-          { text: "支持 MP3 格式" },
-          { text: "支持 pptx 格式，如果是 ppt 格式，请手动转换。" },
-          { text: "支持 ppt、pptx、word and pdf 格式" }
-        ],
-        [
-          { accept: "image/*" },
-          { accept: "video/mp4" },
-          { accept: "audio/mp3" },
-          {
-            accept:
-              "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-          },
-          {
-            accept:
-              "application/pdf, " +
-              "application/vnd.openxmlformats-officedocument.presentationml.presentation, " +
-              "application/vnd.ms-powerpoint, " +
-              "application/msword, " +
-              "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-          },
-          [
-            { fun: "uploadImage" },
-            { fun: "uploadVideo" },
-            { fun: "uploadAudio" },
-            { fun: "uploadDynamic" },
-            { fun: "uploadStatic" }
-          ]
-        ]
+        { title: "上传图片" },
+        { title: "上传视频" },
+        { title: "上传音频" },
+        { title: "资料转网页" },
+        { title: "文档转图片" },
+        { img: "image" },
+        { img: "Video" },
+        { img: "Audio" },
+        { img: "fileTransWeb" },
+        { img: "fileTransImg" },
+        { text: "支持常见格式,可以改变图片大小和位置。" },
+        { text: "支持 MP4 格式" },
+        { text: "支持 MP3 格式" },
+        { text: "支持 pptx 格式，如果是 ppt 格式，请手动转换。" },
+        { text: "支持 ppt、pptx、word and pdf 格式" },
+        { accept: "image/*" },
+        { accept: "video/mp4" },
+        { accept: "audio/mp3" },
+        {
+          accept:
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        },
+        {
+          accept:
+            "application/pdf, " +
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation, " +
+            "application/vnd.ms-powerpoint, " +
+            "application/msword, " +
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        },
+        { fun: "uploadImage" },
+        { fun: "uploadVideo" },
+        { fun: "uploadAudio" },
+        { fun: "uploadDynamic" },
+        { fun: "uploadStatic" }
       ]
     };
   },
