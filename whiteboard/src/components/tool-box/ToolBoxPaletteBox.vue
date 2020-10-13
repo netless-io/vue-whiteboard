3c7d651af647f89206791dec0c71f6ef0bc57c92
netless <meetcheerego@gmail.com>
2020-10-13 10:38:38 +0800

    toolBox

---
+++ whiteboard/src/components/tool-box/ToolBoxPaletteBox.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="palette-box">
+    <div class="tool-box-stroke-box">
+      <div class="tool-box-input-box">
+        <input type="text" class="palette-stroke-slider" />
+      </div>
+      <div class="tool-box-mask-box">
+        <img :src="mask" />
+      </div>
+      <div class="tool-box-under-box-2"></div>
+      <div class="tool-box-under-box"></div>
+    </div>
+    <div class="stroke-script">
+      <div class="stroke-script-text">细</div>
+      <div class="stroke-script-text">粗</div>
+    </div>
+    <div :style="{ width: 156, height: 1, backgroundColor: '#E7E7E7' }"></div>
+
+    <div class="cell-box">
+      <template v-for="item of toolPaletteConfig">
+        <div class="cell-mid-color" :key="item.values">
+          <div class="color" @click="selectColor(newColor)"></div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+// import ToolPaletteConfig from "./ToolPaletteConfig";
+export default {
+  name: "ToolBoxPaletteBox",
+  data() {
+    return {
+      borderColor: "#FFFFF",
+      percentage: this.room.state.memberState.strokeWidth / 32,
+      toolPaletteConfig: [
+        "#FC3A3F",
+        "#FD8343",
+        "#FFDA56",
+        "#9FDF76",
+        "#60E8C6",
+        "#57B2FC",
+        "#4486F6",
+        "#9D27B0",
+        "#DC74FC",
+        "#939AA8",
+        "#1D2129",
+        "#FFFFFF"
+      ]
+    };
+  },
+
+  methods: {
+    setStrokeWidth(event) {
+      const percentage = event.target.value / 32;
+      const strokeWidth = parseInt(event.target.value);
+      this.percentage = percentage;
+      this.room.setMemberState({ strokeWidth: strokeWidth });
+    },
+
+    hexToRgb(hex) {
+      const rgb = [];
+      hex = hex.substr(1);
+      if (hex.length === 3) {
+        hex = hex.replace(/(.)/g, "$1$1");
+      }
+      hex.replace(/../g, color => {
+        rgb.push(parseInt(color, 0x10));
+      });
+      return rgb;
+    },
+
+    selectColor(newColor) {
+      this.room.setMemberState({ strokeColor: newColor });
+    },
+
+    isMatchColor(color) {
+      const { strokeColor } = this.roomState.memberState;
+      return (
+        strokeColor[0] === color[0] &&
+        strokeColor[1] === color[1] &&
+        strokeColor[2] === color[2]
+      );
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "./ToolBoxPaletteBox.less";
+</style>
