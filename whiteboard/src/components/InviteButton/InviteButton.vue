3d707b495b55fee7b759c1d1cea25d02b6ba3e0e
netless <meetcheerego@gmail.com>
2020-10-20 16:57:44 +0800

    invite

--- whiteboard/src/components/InviteButton/InviteButton.vue
+++ whiteboard/src/components/InviteButton/InviteButton.vue
@@ -1,5 +1,27 @@
 <template>
-  <div></div>
+  <el-popopver trigger="click" placement="right-end">
+    <div class="page-controller-cell">
+      <div class="invite-box">
+        <div class="invite-box-title">
+          邀请加入
+        </div>
+        <div
+          :style="{
+            width: '400px',
+            height: '0.5px',
+            backgroundColor: '#E7E7E7'
+          }"
+        ></div>
+        <div class="invite-text-box">
+          <div class="invite-url-box">
+            <span :style="{ whidth: '96px' }">房间号:</span>
+            <!-- input component -->
+          </div>
+        </div>
+      </div>
+      <img :src="this.inviteDisable ? inviteActive : invite" alt="invite" />
+    </div>
+  </el-popopver>
 </template>
 
 <script>
