8c2a76af1d8bba8475c338ae31b9f857277fe7bf
netless <meetcheerego@gmail.com>
2020-10-20 16:56:28 +0800

    fix

---
+++ whiteboard/src/components/InviteButton/InviteButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import copy from "copy-to-clipboard";
+import Identity from "@/Identity";
+import inviteActive from "@/assets/image/invite-active.svg";
+import invite from "@/assets/image/invite.svg";
+export default {
+  name: "InviteButton",
+  data() {
+    return {
+      inviteActive,
+      invite
+    };
+  }
+};
+</script>
+
+<style lang="less">
+@import "./InviteButton.less";
+</style>
