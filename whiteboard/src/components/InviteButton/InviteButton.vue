6fe3f8e13d2a7b579c786d7aff0e14d4f7bce87d
netless <meetcheerego@gmail.com>
2020-10-22 14:45:51 +0800

    inviteButton

--- whiteboard/src/components/InviteButton/InviteButton.vue
+++ whiteboard/src/components/InviteButton/InviteButton.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-popover trigger="click" placement="bottom" width="400">
+  <el-popover
+    trigger="click"
+    placement="bottom"
+    width="400"
+    @click="onVisibleChange"
+  >
     <div class="invite-box">
       <div class="invite-box-title">
         邀请加入
@@ -17,13 +22,21 @@
             >房间号：</span
           >
           <el-input :value="uuid" slot="append">
-            <el-button slot="append" icon="el-icon-document-copy"></el-button
+            <el-button
+              @click="copyUuidSuccess"
+              slot="append"
+              icon="el-icon-document-copy"
+            ></el-button
           ></el-input>
         </div>
         <div class="invite-url-box">
           <span :style="{ whidth: '96px' }">加入链接：</span>
           <el-input :value="uuid" slot="append">
-            <el-button slot="append" icon="el-icon-document-copy"></el-button
+            <el-button
+              @click="copyLinkSuccess"
+              slot="append"
+              icon="el-icon-document-copy"
+            ></el-button
           ></el-input>
         </div>
       </div>
@@ -32,12 +45,18 @@
           type="primary"
           size="medium"
           :style="{ width: '164px', height: '40px' }"
+          @click="copySuccess"
           >复制</el-button
         >
       </div>
     </div>
 
-    <img slot="reference" :src="invite" alt="invite" />
+    <img
+      slot="reference"
+      @click="handleInvite"
+      :src="inviteDisable ? inviteActive : invite"
+      alt="invite"
+    />
   </el-popover>
 </template>
 
@@ -57,15 +76,48 @@ export default {
   data() {
     return {
       inviteActive,
-      invite
+      invite,
+      inviteDisable: false,
+      isLocal: location.hostname === "localhost"
     };
   },
   methods: {
     handleCopy() {
+      this.handleInvite();
       copy(
         `房间号：${this.uuid}\n加入链接：https://demo.netless.link/whiteboard/${Identity.joiner}/${this.uuid}/`
       );
-      // message
+    },
+    onVisibleChange(event) {
+      if (event) {
+        this.inviteDisable = true;
+      } else {
+        this.inviteDisable = false;
+      }
+    },
+    handleInvite() {
+      this.inviteDisable = !this.inviteDisable;
+    },
+    copySuccess() {
+      this.$message({
+        message: "已经将信息复制到剪贴板",
+        type: "success",
+        center: true
+      });
+    },
+    copyUuidSuccess() {
+      this.$message({
+        message: "已经将 uuid 黏贴到剪贴板",
+        type: "success",
+        center: true
+      });
+    },
+    copyLinkSuccess() {
+      this.$message({
+        message: "已经将链接黏贴到剪贴板",
+        type: "success",
+        center: true
+      });
     }
   }
 };
