4e32b6ce661b1ef777261bd68785beb43f9266da
cheerchen <meetcheerego@gmail.com>
2020-11-09 18:31:59 +0800

    add config

--- whiteboard/src/main.js
+++ whiteboard/src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "normalize.css";
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
